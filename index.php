d1b027607615a340dfdedae6270712af4b2541b2
Alec Smecher <alec@smecher.bc.ca>
2020-02-07 16:50:58 -0800

    pkp/pkp-lib#5488 Update copyright dates and GPLv2 license to GPLv3

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 /**
  * @file plugins/viewableFile/pdfJsViewer/index.php
  *
- * Copyright (c) 2013-2019 Simon Fraser University
- * Copyright (c) 2003-2019 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Copyright (c) 2013-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @ingroup plugins_viewableFile_pdfJsViewer
  * @brief Wrapper for pdf.js-based viewer.
