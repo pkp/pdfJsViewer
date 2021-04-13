5e515cc84ba030379d51ab4e23ccc175872a6a65
Nate Wright <natew@notthisway.com>
2021-04-13 17:05:08 +0100

    pkp/pkp-lib#5678 Format all PHP files to PSR12

--- PdfJsViewerPlugin.inc.php
+++ PdfJsViewerPlugin.inc.php
@@ -14,134 +14,147 @@
 
 import('lib.pkp.classes.plugins.GenericPlugin');
 
-class PdfJsViewerPlugin extends GenericPlugin {
-	/**
-	 * @copydoc Plugin::register()
-	 */
-	function register($category, $path, $mainContextId = null) {
-		if (parent::register($category, $path, $mainContextId)) {
-			if ($this->getEnabled($mainContextId)) {
-				// For OPS
-				HookRegistry::register('PreprintHandler::view::galley', array($this, 'submissionCallback'), HOOK_SEQUENCE_LAST);
-				// For OJS
-				HookRegistry::register('ArticleHandler::view::galley', array($this, 'submissionCallback'), HOOK_SEQUENCE_LAST);
-				HookRegistry::register('IssueHandler::view::galley', array($this, 'issueCallback'), HOOK_SEQUENCE_LAST);
-				AppLocale::requireComponents(LOCALE_COMPONENT_APP_COMMON);
-			}
-			return true;
-		}
-		return false;
-	}
+class PdfJsViewerPlugin extends GenericPlugin
+{
+    /**
+     * @copydoc Plugin::register()
+     *
+     * @param null|mixed $mainContextId
+     */
+    public function register($category, $path, $mainContextId = null)
+    {
+        if (parent::register($category, $path, $mainContextId)) {
+            if ($this->getEnabled($mainContextId)) {
+                // For OPS
+                HookRegistry::register('PreprintHandler::view::galley', [$this, 'submissionCallback'], HOOK_SEQUENCE_LAST);
+                // For OJS
+                HookRegistry::register('ArticleHandler::view::galley', [$this, 'submissionCallback'], HOOK_SEQUENCE_LAST);
+                HookRegistry::register('IssueHandler::view::galley', [$this, 'issueCallback'], HOOK_SEQUENCE_LAST);
+                AppLocale::requireComponents(LOCALE_COMPONENT_APP_COMMON);
+            }
+            return true;
+        }
+        return false;
+    }
 
-	/**
-	 * Install default settings on context creation.
-	 * @return string
-	 */
-	function getContextSpecificPluginSettingsFile() {
-		return $this->getPluginPath() . '/settings.xml';
-	}
+    /**
+     * Install default settings on context creation.
+     *
+     * @return string
+     */
+    public function getContextSpecificPluginSettingsFile()
+    {
+        return $this->getPluginPath() . '/settings.xml';
+    }
 
-	/**
-	 * @copydoc Plugin::getDisplayName
-	 */
-	function getDisplayName() {
-		return __('plugins.generic.pdfJsViewer.name');
-	}
+    /**
+     * @copydoc Plugin::getDisplayName
+     */
+    public function getDisplayName()
+    {
+        return __('plugins.generic.pdfJsViewer.name');
+    }
 
-	/**
-	 * @copydoc Plugin::getDescription
-	 */
-	function getDescription() {
-		return __('plugins.generic.pdfJsViewer.description');
-	}
+    /**
+     * @copydoc Plugin::getDescription
+     */
+    public function getDescription()
+    {
+        return __('plugins.generic.pdfJsViewer.description');
+    }
 
-	/**
-	 * Callback that renders the submission galley.
-	 * @param $hookName string
-	 * @param $args array
-	 * @return boolean
-	 */
-	function submissionCallback($hookName, $args) {
-		$request =& $args[0];
-		$application = Application::get();
-		switch ($application->getName()) {
-			case 'ojs2':
-				$issue =& $args[1];
-				$galley =& $args[2];
-				$submission =& $args[3];
-				$submissionNoun = 'article';
-				break;
-			case 'ops':
-				$galley =& $args[1];
-				$submission =& $args[2];
-				$submissionNoun = 'preprint';
-				$issue = null;
-				break;
-			default: throw new Exception('Unknown application!');
-		}
+    /**
+     * Callback that renders the submission galley.
+     *
+     * @param $hookName string
+     * @param $args array
+     *
+     * @return boolean
+     */
+    public function submissionCallback($hookName, $args)
+    {
+        $request = & $args[0];
+        $application = Application::get();
+        switch ($application->getName()) {
+            case 'ojs2':
+                $issue = & $args[1];
+                $galley = & $args[2];
+                $submission = & $args[3];
+                $submissionNoun = 'article';
+                break;
+            case 'ops':
+                $galley = & $args[1];
+                $submission = & $args[2];
+                $submissionNoun = 'preprint';
+                $issue = null;
+                break;
+            default: throw new Exception('Unknown application!');
+        }
 
-		if (!$galley) {
-			return false;
-		}
+        if (!$galley) {
+            return false;
+        }
 
-		$submissionFile = $galley->getFile();
-		if ($submissionFile->getData('mimetype') === 'application/pdf') {
-			$galleyPublication = null;
-			foreach ($submission->getData('publications') as $publication) {
-				if ($publication->getId() === $galley->getData('publicationId')) {
-					$galleyPublication = $publication;
-					break;
-				}
-			}
-			$templateMgr = TemplateManager::getManager($request);
-			$templateMgr->assign(array(
-				'displayTemplateResource' => $this->getTemplateResource('display.tpl'),
-				'pluginUrl' => $request->getBaseUrl() . '/' . $this->getPluginPath(),
-				'galleyFile' => $submissionFile,
-				'issue' => $issue,
-				'submission' => $submission,
-				'submissionNoun' => $submissionNoun,
-				'bestId' => $submission->getBestId(),
-				'galley' => $galley,
-				'currentVersionString' => $application->getCurrentVersion()->getVersionString(false),
-				'isLatestPublication' => $submission->getData('currentPublicationId') === $galley->getData('publicationId'),
-				'galleyPublication' => $galleyPublication,
-			));
-			$templateMgr->display($this->getTemplateResource('submissionGalley.tpl'));
-			return true;
-		}
+        $submissionFile = $galley->getFile();
+        if ($submissionFile->getData('mimetype') === 'application/pdf') {
+            $galleyPublication = null;
+            foreach ($submission->getData('publications') as $publication) {
+                if ($publication->getId() === $galley->getData('publicationId')) {
+                    $galleyPublication = $publication;
+                    break;
+                }
+            }
+            $templateMgr = TemplateManager::getManager($request);
+            $templateMgr->assign([
+                'displayTemplateResource' => $this->getTemplateResource('display.tpl'),
+                'pluginUrl' => $request->getBaseUrl() . '/' . $this->getPluginPath(),
+                'galleyFile' => $submissionFile,
+                'issue' => $issue,
+                'submission' => $submission,
+                'submissionNoun' => $submissionNoun,
+                'bestId' => $submission->getBestId(),
+                'galley' => $galley,
+                'currentVersionString' => $application->getCurrentVersion()->getVersionString(false),
+                'isLatestPublication' => $submission->getData('currentPublicationId') === $galley->getData('publicationId'),
+                'galleyPublication' => $galleyPublication,
+            ]);
+            $templateMgr->display($this->getTemplateResource('submissionGalley.tpl'));
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Callback that renders the issue galley.
-	 * @param $hookName string
-	 * @param $args array
-	 * @return boolean
-	 */
-	function issueCallback($hookName, $args) {
-		$request =& $args[0];
-		$issue =& $args[1];
-		$galley =& $args[2];
+    /**
+     * Callback that renders the issue galley.
+     *
+     * @param $hookName string
+     * @param $args array
+     *
+     * @return boolean
+     */
+    public function issueCallback($hookName, $args)
+    {
+        $request = & $args[0];
+        $issue = & $args[1];
+        $galley = & $args[2];
 
-		$templateMgr = TemplateManager::getManager($request);
-		if ($galley && $galley->getFileType() == 'application/pdf') {
-			$application = Application::get();
-			$templateMgr->assign(array(
-				'displayTemplateResource' => $this->getTemplateResource('display.tpl'),
-				'pluginUrl' => $request->getBaseUrl() . '/' . $this->getPluginPath(),
-				'galleyFile' => $galley->getFile(),
-				'issue' => $issue,
-				'galley' => $galley,
-				'currentVersionString' => $application->getCurrentVersion()->getVersionString(false),
-				'isLatestPublication' => true,
-			));
-			$templateMgr->display($this->getTemplateResource('issueGalley.tpl'));
-			return true;
-		}
+        $templateMgr = TemplateManager::getManager($request);
+        if ($galley && $galley->getFileType() == 'application/pdf') {
+            $application = Application::get();
+            $templateMgr->assign([
+                'displayTemplateResource' => $this->getTemplateResource('display.tpl'),
+                'pluginUrl' => $request->getBaseUrl() . '/' . $this->getPluginPath(),
+                'galleyFile' => $galley->getFile(),
+                'issue' => $issue,
+                'galley' => $galley,
+                'currentVersionString' => $application->getCurrentVersion()->getVersionString(false),
+                'isLatestPublication' => true,
+            ]);
+            $templateMgr->display($this->getTemplateResource('issueGalley.tpl'));
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
-
